a2e94f7af614f4b19a9bac755af67acf1e08b985
Jacinto Mba Cantero <jacmba@gmail.com>
2020-02-27 13:33:43 +0000

    Slide pics comments

--- components/Carrousel.vue
+++ components/Carrousel.vue
@@ -3,8 +3,12 @@
     <b-carousel controls indicators :interval="2000">
       <b-carousel-slide img-src="/carrousel1.jpg"></b-carousel-slide>
       <b-carousel-slide img-src="/carrousel2.jpg"></b-carousel-slide>
-      <b-carousel-slide img-src="/carrousel3.jpg"></b-carousel-slide>
-      <b-carousel-slide img-src="/carrousel4.jpg"></b-carousel-slide>
+      <b-carousel-slide img-src="/carrousel3.jpg">
+        Porque sin los terceros tiempos no seríamos nadie
+      </b-carousel-slide>
+      <b-carousel-slide img-src="/carrousel4.jpg">
+        Asaderassso!!
+      </b-carousel-slide>
     </b-carousel>
   </div>
 </template>
